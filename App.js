3120364c932ff7e864d51359a5ec02d1787e5672
JackieTree <jackiejeep@gmail.com>
2018-07-30 12:58:48 -0700

    minor change

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import SendRecPage from '/src/pages/SendRecPage.js';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        { SendRecPage }
+        <text>{ SendRecPage }</text>
         <Route exact path="/src/pages/:SendRecID" component={SendRecPage} />
         <Text>Open up App.js to start working on your app!</Text>
       </View>
